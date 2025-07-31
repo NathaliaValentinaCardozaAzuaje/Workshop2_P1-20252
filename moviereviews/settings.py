d945c7836911aed9ea51b525b4c71c35ff5ce4f0
NathaliaValentinaCardozaAzuaje <nvcardozaa@eafit.edu.co>
2025-07-30 21:06:21 -0500

    busqueda de peliculas

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -10,11 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from pathlib import Path
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
